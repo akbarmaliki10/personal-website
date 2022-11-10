cdd292541aeb4e172615b628032be72bbf409487
akbarmaliki109 <akbarmaliki109@gmail.com>
2022-11-10 21:23:50 +0700

    Migrate to nodejs

--- assets/demo.js
+++ assets/demo.js
@@ -5,34 +5,34 @@ let projectD = document.querySelector("#projectd");
 
 
 projectA.addEventListener('mouseover', function(){
-    projectA.src="assets/images/demo.gif"
+    projectA.src="images/demo.gif"
 });
 
 projectA.addEventListener('mouseleave', function(){
-    projectA.src="assets/images/projectA.png"
+    projectA.src="images/projectA.png"
 });
 
 
 projectB.addEventListener('mouseover', function(){
-    projectB.src="assets/images/demo2.gif"
+    projectB.src="images/demo2.gif"
 });
 
 projectB.addEventListener('mouseleave', function(){
-    projectB.src="assets/images/projectB.png"
+    projectB.src="images/projectB.png"
 });
 
 projectC.addEventListener('mouseover', function(){
-    projectC.src="assets/images/demo3.gif"
+    projectC.src="images/demo3.gif"
 });
 
 projectC.addEventListener('mouseleave', function(){
-    projectC.src="assets/images/projectC.png"
+    projectC.src="images/projectC.png"
 });
 
 projectD.addEventListener('mouseover', function(){
-    projectD.src="assets/images/demo4.gif"
+    projectD.src="images/demo4.gif"
 });
 
 projectD.addEventListener('mouseleave', function(){
-    projectD.src="assets/images/projectD.png"
+    projectD.src="images/projectD.png"
 });
